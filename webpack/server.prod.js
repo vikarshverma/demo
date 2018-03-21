5f0283d5b6691551bdab74c42ebb8aae96cb19e3
Vivek Pal <vivek.pal@durlabhcomputers.com>
2018-03-22 02:47:53 +0530

    testing.

---
+++ webpack/server.prod.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  context: path.join(__dirname, '../server'),
+  devtool: 'source-map',
+  entry: [
+    './routes/index.js',
+  ],
+  target: 'node',
+  output: {
+    path: path.join(__dirname, '../server/bin'),
+    filename: './server.js',
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'stage-1'],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images/',
+              emitFile: false,
+            }  
+          }
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+  ]
+};
